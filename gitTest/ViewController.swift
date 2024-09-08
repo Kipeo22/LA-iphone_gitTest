b2e524f8e8cd3925c7fe6e9d4c63a1ef75a875b2
Kipeo <kp225048@gmail.com>
2024-09-08 23:17:37 +0900

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by x22086xx on 2024/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
